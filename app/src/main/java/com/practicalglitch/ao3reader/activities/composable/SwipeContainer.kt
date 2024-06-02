d0eff2a2e66b7ff105278b84c0ba42e3c585d231
practicalglitch <169734687+practicalglitch@users.noreply.github.com>
2024-06-02 15:46:11 -0400

    Refactor SwipeContainer for compose 1.2.x

--- app/src/main/java/com/practicalglitch/ao3reader/activities/composable/SwipeContainer.kt
+++ app/src/main/java/com/practicalglitch/ao3reader/activities/composable/SwipeContainer.kt
@@ -3,12 +3,10 @@ package com.practicalglitch.ao3reader.activities.composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.DismissState
-import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SwipeToDismiss
-import androidx.compose.material3.rememberDismissState
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,7 +16,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun <T> SwipeContainer(
@@ -27,52 +24,37 @@ fun <T> SwipeContainer(
 	backgroundColor: Color,
 	backgroundContent: @Composable () -> Unit,
 	content: @Composable (T) -> Unit
-){
-	var isRemoved by remember{
-		mutableStateOf(false)
-	}
-	val state = rememberDismissState(
-		confirmValueChange = {value -> if(value == DismissValue.DismissedToStart) {
-			isRemoved = true
-			true
-		} else
-		false}
-	)
-	
-	if(isRemoved && state.currentValue != DismissValue.Default) {
-		onSwipe(item)
-	}
-	if(state.currentValue != DismissValue.Default)
-		LaunchedEffect(Unit) {
-			state.reset()
-		}
-	
-	SwipeToDismiss(state = state,
-		background = { SwipeItem(swipeState = state, color = backgroundColor) {
-			backgroundContent()
-		} },
-		dismissContent = { content(item) },
-		directions = setOf(DismissDirection.EndToStart)
-	)
-}
+) {
+	var isSwiped by remember { mutableStateOf(false) }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SwipeItem(
-	swipeState: DismissState,
-	color: Color,
-	content: @Composable () -> Unit
-){
-	val displayedColor = if(swipeState.dismissDirection == DismissDirection.EndToStart)
-		color
-	else
-		Color.Transparent
+	val swipeToDismissState = rememberSwipeToDismissBoxState(
+		confirmValueChange = {
+			isSwiped = true
+			true
+		}
+	)
 	
-	Box (
-		modifier = Modifier
-			.fillMaxSize()
-			.background(displayedColor)
-	){
-		content()
+	if(swipeToDismissState.currentValue != SwipeToDismissBoxValue.Settled) {
+		if(isSwiped) {
+			onSwipe(item)
+			isSwiped = false
+		}
+		
+		LaunchedEffect(Unit) {
+			swipeToDismissState.reset()
+		}
+	}
+	
+	SwipeToDismissBox(
+		state = swipeToDismissState,
+		backgroundContent = {
+			Box(modifier = Modifier
+				.fillMaxSize()
+				.background(backgroundColor)){
+				backgroundContent()
+			}
+		},
+		enableDismissFromStartToEnd = false) {
+		content(item)
 	}
 }
